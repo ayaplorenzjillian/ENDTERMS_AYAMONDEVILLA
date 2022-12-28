b6e42bbf08ddc2894bfc5326305f6c890f3b7324
Jillian Montinola <100033705+ayaplorenzjillian@users.noreply.github.com>
2022-12-28 21:03:56 +0800

    Add files via upload

---
+++ ENDTERMS_AYAP_MONDEJAR_VILLA/src/main/java/com/mycompany/endterms_ayap_mondejar_villa/ENDTERMS_AYAP_MONDEJAR_VILLA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.endterms_ayap_mondejar_villa;
+
+/**
+ *
+ * @author jillianayap
+ */
+public class ENDTERMS_AYAP_MONDEJAR_VILLA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
